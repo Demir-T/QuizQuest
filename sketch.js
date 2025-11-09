da457110b4a94c73fa08d7579d2805fa6a451126
Demir-T <demir.topoyan@gmail.com>
2025-11-10 01:27:30 +0300

    Refactor question checks to use else if

--- sketch.js
+++ sketch.js
@@ -3,7 +3,7 @@ let number;
 let question;
 
 function preload() {
-  pic = loadImage("assets/fruits.png"); // Başlangıç görseli
+  pic = loadImage("assets/fruits.png"); // İlk görsel
 }
 
 function setup() {
@@ -31,87 +31,78 @@ function mouseClicked() {
       pic = loadImage("assets/australia.gif");
       question = "Where is the capital?";
       number++;
-      return;
     } else output("You missed");
   }
 
   // Question 2
-  if (number === 2) {
+  else if (number === 2) {
     if (mouseX > 280 && mouseX < 350 && mouseY > 280 && mouseY < 350) {
       output("Right - Canberra");
       pic = loadImage("assets/sports.jpg");
       question = "Find American Football";
       number++;
-      return;
     } else output("No, try again");
   }
 
   // Question 3
-  if (number === 3) {
+  else if (number === 3) {
     if (mouseX > 100 && mouseX < 205 && mouseY > 250 && mouseY < 360) {
       output("Correct");
       pic = loadImage("assets/drinks.jpg");
       question = "Which one is coconut juice?";
       number++;
-      return;
     } else output("No");
   }
 
   // Question 4
-  if (number === 4) {
+  else if (number === 4) {
     if (mouseX > 250 && mouseX < 350 && mouseY > 50 && mouseY < 150) {
       output("Correct");
       pic = loadImage("assets/mc.jpg");
       question = "Which one is the Enderman?";
       number++;
-      return;
     } else output("No, try again");
   }
 
   // Question 5
-  if (number === 5) {
+  else if (number === 5) {
     if (mouseX > 400 && mouseX < 460 && mouseY > 50 && mouseY < 150) {
       output("Correct!");
       pic = loadImage("assets/space.jpg");
       question = "Which planet is known as the Red Planet?";
       number++;
-      return;
     } else output("No, try again");
   }
 
-  // Question 6 
-  if (number === 6) {
+  // Question 6
+  else if (number === 6) {
     if (mouseX > 100 && mouseX < 200 && mouseY > 100 && mouseY < 200) {
       output("Correct! It's Mars");
       pic = loadImage("assets/animals.jpg");
       question = "Click on the lion";
       number++;
-      return;
     } else output("No, try again");
   }
 
-  // Question 7 
-  if (number === 7) {
+  // Question 7
+  else if (number === 7) {
     if (mouseX > 300 && mouseX < 400 && mouseY > 150 && mouseY < 250) {
       output("Correct! Lion spotted");
       pic = loadImage("assets/countries.jpg");
       question = "Which country has the Eiffel Tower?";
       number++;
-      return;
     } else output("No, try again");
   }
 
-  // Question 8 
-  if (number === 8) {
+  // Question 8 – Final
+  else if (number === 8) {
     if (mouseX > 400 && mouseX < 500 && mouseY > 200 && mouseY < 300) {
-      output("Congratulations! You completed QuizQuest!");
+      output("🎉 Congratulations! You completed QuizQuest!");
       noLoop();
-      return;
     } else output("No, try again");
   }
 }
 
-// Alert mesaj fonksiyonu
 function output(message) {
   alert(message);
 }
